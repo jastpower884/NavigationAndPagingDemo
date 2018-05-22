7aed9917bd3aa81837abfc0ce9e24b16e5f3b944
Jast <jast.lai@yxunistar.com>
2018-05-22 14:14:59 +0800

    init branch

--- app/src/main/java/com/jastzeonic/navigationandpagingdemo/MainActivity.kt
+++ app/src/main/java/com/jastzeonic/navigationandpagingdemo/MainActivity.kt
@@ -1,16 +1,7 @@
 package com.jastzeonic.navigationandpagingdemo
 
-import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.google.gson.Gson
-import com.jastzeonic.navigationandpagingdemo.database.GameInfoRepository
-import com.jastzeonic.navigationandpagingdemo.database.RepositoryProvider
-import com.jastzeonic.navigationandpagingdemo.model.Data
-import com.jastzeonic.navigationandpagingdemo.model.GameInfoModel
-import com.jastzeonic.navigationandpagingdemo.model.InfoModel
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/jastzeonic/navigationandpagingdemo/model/Data.kt
+++
@@ -1,6 +0,0 @@
-package com.jastzeonic.navigationandpagingdemo.model
-
-class Data {
-
-    var game_list = ArrayList<InfoModel>()
-}
